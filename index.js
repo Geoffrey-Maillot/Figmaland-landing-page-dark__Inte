fee4af538e6eb37af15bba316ca641c648594da7
G-M <geoffrey.maillot@gmail.com>
2021-02-04 20:42:58 +0100

    Section content en cour

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-document.getElementById(`responsive-menu`).addEventListener(`click`, (e) => {
-document.getElementById(`nav`).classList.toggle(`active`)
-document.querySelector(`body`).classList.toggle(`scroll-none`)
-})
+ var app = {
+    init: function () {
+      
+      document.getElementById(`responsive-menu`).addEventListener(`click`, (e) => {
+        document.getElementById(`nav`).classList.toggle(`active`)
+        document.querySelector(`body`).classList.toggle(`scroll-none`)
+        })
+        
+        document.getElementById(`close-nav`).addEventListener(`click`, (e) => {
+          document.getElementById(`nav`).classList.toggle(`active`)
+          document.querySelector(`body`).classList.toggle(`scroll-none`)
+          })
 
-document.getElementById(`close-nav`).addEventListener(`click`, (e) => {
-  document.getElementById(`nav`).classList.toggle(`active`)
-  document.querySelector(`body`).classList.toggle(`scroll-none`)
-  })
+    }
+  };
+  
+  document.addEventListener('DOMContentLoaded', app.init);
