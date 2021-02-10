87cfba9f50133ecf250ba7840b1bcdc1615a15dd
G-M <geoffrey.maillot@gmail.com>
2021-02-10 08:52:10 +0100

    rajout défilement ancre contact avec défilement fluide

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
- var app = {
-    init: function () {
-      
-      document.getElementById(`responsive-menu`).addEventListener(`click`, (e) => {
-        document.getElementById(`nav`).classList.toggle(`active`)
-        document.querySelector(`body`).classList.toggle(`scroll-none`)
-        })
-        
-        document.getElementById(`close-nav`).addEventListener(`click`, (e) => {
-          document.getElementById(`nav`).classList.toggle(`active`)
-          document.querySelector(`body`).classList.toggle(`scroll-none`)
-          })
+var app = {
+  init: function () {
+    document
+      .getElementById(`responsive-menu`)
+      .addEventListener(`click`, (e) => {
+        document.getElementById(`nav`).classList.toggle(`active`);
+        document.querySelector(`body`).classList.toggle(`scroll-none`);
+      });
 
-    }
-  };
-  
-  document.addEventListener('DOMContentLoaded', app.init);
+    document.getElementById(`close-nav`).addEventListener(`click`, (e) => {
+      document.getElementById(`nav`).classList.toggle(`active`);
+      document.querySelector(`body`).classList.toggle(`scroll-none`);
+    });
+
+    document.getElementById(`#contact`).addEventListener(`click`, (e) => {
+      document.getElementById(`nav`).classList.toggle(`active`);
+      document.querySelector(`body`).classList.toggle(`scroll-none`);
+    });
+  },
+};
+
+document.addEventListener("DOMContentLoaded", app.init);
